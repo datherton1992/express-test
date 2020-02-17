deeb4b2d3bd9f0343ab243d3783f2cd54354a5a8
David Atherton <davidatherton92@gmail.com>
2020-02-17 20:24:55 +0000

    Refactor creation of server application

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => { console.log('Listening on port 3000');})
+app.listen(3000, () => console.log('Listening on port 3000'));
